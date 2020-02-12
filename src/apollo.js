8f7be2c5e0075c6cb3e016b3cd005aae049f0268
jaemin_seo <sjm@neochart.co.kr>
2020-02-12 12:41:05 +0900

    graduation

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,18 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "https://moviesql.now.sh/"
+  uri: "http://localhost:3001/",
+  resolvers: {
+    Movie: {
+      isLiked: () => false
+    },
+    Mutation: {
+      toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+        // console.log(id, cache);
+        cache.writeData({ id: `Movie:${id}`, data: { isLiked: !isLiked } });
+      }
+    }
+  }
 });
 
 export default client;
